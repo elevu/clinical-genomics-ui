9bf4b97a84492a34f3ecc5db44824d995f0a06c7
elevu <eleonora.maccacaro@gmail.com>
2020-10-17 16:56:29 +0200

    Add Redux, Google Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.style.css'
-import { Layout, Menu } from 'antd'
+import { Button, Layout, Menu } from 'antd'
 import { Routes } from './components/Routes'
 import { Link, useLocation } from 'react-router-dom'
+import { initGoogleClient, signOutGoogleClient } from './api/GoogleClient'
+import {
+  setSettings as setSettingsAction,
+  resetSettings as resetSettingsAction,
+} from './domain/settings/slice'
+import { RootState } from './domain/rootReducer'
+import { connect } from 'react-redux'
+import { compose } from '@reduxjs/toolkit'
 
 export interface AppProps {
   name: string
@@ -10,36 +18,58 @@ export interface AppProps {
 
 const { Header, Content, Footer } = Layout
 
-export const App: React.FC<AppProps> = (props: AppProps) => (
-  <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" selectedKeys={[useLocation().pathname]}>
-        <Menu.Item key="/">
-          <Link to="/">
-            <span>Home</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/trailblazer">
-          <Link to="/trailblazer">
-            <span>Trailblazer</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/genotype">
-          <Link to="/genotype">
-            <span>Genotype</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/vogue">
-          <Link to="/vogue">
-            <span>Vogue</span>
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </Header>
-    <Content>
-      <Routes />
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Clinical Genomics ©2020</Footer>
-  </Layout>
-)
+const mapDispatch = {
+  setSettings: setSettingsAction,
+  resetSettings: resetSettingsAction,
+} as const
+const mapState = ({ settings }: RootState) => ({ settings } as const)
+
+type Props = ReturnType<typeof mapState> & typeof mapDispatch
+
+const AppComponent = ({ settings, setSettings, resetSettings }: Props) => {
+  const setStore = () => {
+    initGoogleClient(setSettings)
+  }
+  const handleSignOut = () => {
+    signOutGoogleClient(resetSettings)
+  }
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" selectedKeys={[useLocation().pathname]}>
+          <Menu.Item key="/">
+            <Link to="/">
+              <span>Home</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/trailblazer">
+            <Link to="/trailblazer">
+              <span>Trailblazer</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/genotype">
+            <Link to="/genotype">
+              <span>Genotype</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/vogue">
+            <Link to="/vogue">
+              <span>Vogue</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content>
+        <Routes />
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Clinical Genomics ©2020
+        <Button onClick={() => setStore()}>Login</Button>
+        <Button onClick={() => handleSignOut()}>Sign Out</Button>
+      </Footer>
+    </Layout>
+  )
+}
+
+export const App = compose(connect(mapState, mapDispatch))(AppComponent)
