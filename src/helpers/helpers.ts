cf59dfe84beb1b1fcd0a2300ab8e0b374bf908c6
elevu <eleonora.maccacaro@gmail.com>
2020-10-18 19:50:23 +0200

    Add test

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,7 +1,7 @@
 import { presets } from './constants'
 
-// Returns an ANTD preset color given any string
-export const colorFromString = (str: string) => {
-  const numberA = parseInt(String(str.length).charAt(0))
-  return presets[numberA]
+// Returns an Antd preset color given any string
+export const colorFromString = (str: string, colorList = presets) => {
+  const numberFromString = parseInt(String(str.length).charAt(0))
+  return colorList[numberFromString]
 }
