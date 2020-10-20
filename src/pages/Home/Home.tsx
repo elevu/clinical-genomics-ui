1d4201599b63e7600a57fc19b923cb1a676b94bf
elevu <eleonora.maccacaro@gmail.com>
2020-10-20 15:11:58 +0200

    Improve general style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import './Home.style.css'
-import MainLogo from 'assets/cg-big-logo.png'
+import MainLogo from 'assets/cg-big-logo.svg'
 
 export const Home = () => (
-  <div>
+  <div className="home-container">
+    <h1>Welcome to the Clinical Genomics Internal Portal!</h1>
+    <p>Login with your SciLifeLab Google account to access the portal.</p>
+    <br />
     <img className="main-logo" src={MainLogo}></img>
   </div>
 )
